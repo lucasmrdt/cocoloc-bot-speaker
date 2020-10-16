7e14d68d943839906f0c45b062b730df7e176ef2
lucasmrdt <lucasmrdt@localhost.localdomain>
2020-10-16 07:51:32 +0200

    improve bot

--- bot.py
+++ bot.py
@@ -1,25 +1,27 @@
-import pyttsx3
 import time
 import platform
 import redis
+import playsound
+import requests
 
 REDIS_URL = 'redis://h:p536dfe7c3cbbcc9c049d0f0d127697e6fb9c63e17b15b7ab2ad50b22d30088fb@ec2-54-170-172-162.eu-west-1.compute.amazonaws.com:12319'
-VOICE_BY_SYSTEM = {
-    'Darwin': 'com.apple.speech.synthesis.voice.amelie'
-}
-
 
 redis_server = redis.Redis(host='ec2-54-170-172-162.eu-west-1.compute.amazonaws.com', port='12319',
                            password='p536dfe7c3cbbcc9c049d0f0d127697e6fb9c63e17b15b7ab2ad50b22d30088fb')
 
-engine = pyttsx3.init()
-try:
-    system = platform.system()
-    assert system in VOICE_BY_SYSTEM
-    engine.setProperty('voice', VOICE_BY_SYSTEM[system])
-except AssertionError:
-    print('warning: no voice configured for your system')
+def create_audio(msg):
+    res = requests.post('https://ttsmp3.com/makemp3_new.php', data={'msg': msg, 'lang': 'Celine', 'source': 'ttsmp3'})
+    data = res.json()
+    if data['Error']:
+        raise Exception('error while creating audio file')
+    return data['URL']
 
+def speak(msg):
+    try:
+        url = create_audio(msg)
+        playsound.playsound(url)
+    except Exception as e:
+        print(f'error: fail to speak - {e}')
 
 def bot():
     print('waiting messages ...')
@@ -28,10 +30,12 @@ def bot():
         if response:
             msg = response.decode('utf-8')
             print(f'saying: "{msg}"')
-            engine.say(msg)
-            engine.runAndWait()
+            speak(msg)
         time.sleep(1)
 
 
 if __name__ == '__main__':
-    bot()
+    try:
+        bot()
+    except KeyboardInterrupt:
+        print('bye !')
