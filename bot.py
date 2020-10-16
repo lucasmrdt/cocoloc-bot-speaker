a991e79ede5e280b525ba4c858f72a90d5573ce1
lucasmrdt <lucasmrdt@localhost.localdomain>
2020-10-16 16:37:51 +0200

    uprade

--- bot.py
+++ bot.py
@@ -1,37 +1,42 @@
+from google.cloud import texttospeech
 import time
 import platform
 import redis
 import playsound
-import requests
 
 REDIS_URL = 'redis://h:p536dfe7c3cbbcc9c049d0f0d127697e6fb9c63e17b15b7ab2ad50b22d30088fb@ec2-54-170-172-162.eu-west-1.compute.amazonaws.com:12319'
 
 redis_server = redis.Redis(host='ec2-54-170-172-162.eu-west-1.compute.amazonaws.com', port='12319',
                            password='p536dfe7c3cbbcc9c049d0f0d127697e6fb9c63e17b15b7ab2ad50b22d30088fb')
+client = texttospeech.TextToSpeechClient()
+voice = texttospeech.VoiceSelectionParams(
+    language_code="fr-FR-Standard-A", ssml_gender=texttospeech.SsmlVoiceGender.FEMALE
+)
+audio_config = texttospeech.AudioConfig(
+    audio_encoding=texttospeech.AudioEncoding.MP3
+)
 
-def create_audio(msg):
-    res = requests.post('https://ttsmp3.com/makemp3_new.php', data={'msg': msg, 'lang': 'Celine', 'source': 'ttsmp3'})
-    data = res.json()
-    if data['Error']:
-        raise Exception('error while creating audio file')
-    return data['URL']
+def create_voice(msg):
+    synthesis_input = texttospeech.SynthesisInput(text=msg)
+    response = client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
+    with open("voice.mp3", "wb") as out:
+        out.write(response.audio_content)
 
 def speak(msg):
     try:
-        url = create_audio(msg)
-        playsound.playsound(url)
+        create_voice(msg)
+        playsound.playsound('./voice.mp3')
     except Exception as e:
         print(f'error: fail to speak - {e}')
 
 def bot():
     print('waiting messages ...')
     while True:
-        response = redis_server.lpop('msg')
+        _, response = redis_server.brpop('msg', timeout=0)
         if response:
             msg = response.decode('utf-8')
             print(f'saying: "{msg}"')
-            speak(msg)
-        time.sleep(1)
+            speak(f'a. {msg}')
 
 
 if __name__ == '__main__':
