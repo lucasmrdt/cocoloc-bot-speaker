35a863521d5a0b7a7ec3cdbe110d6382e487c4d3
lucasmrdt <lucasmrdt@localhost.localdomain>
2020-10-17 18:48:15 +0200

    upgrade

--- bot.py
+++ bot.py
@@ -3,6 +3,7 @@ import time
 import platform
 import redis
 import playsound
+import datetime
 
 REDIS_URL = 'redis://h:p536dfe7c3cbbcc9c049d0f0d127697e6fb9c63e17b15b7ab2ad50b22d30088fb@ec2-54-170-172-162.eu-west-1.compute.amazonaws.com:12319'
 
@@ -16,6 +17,13 @@ audio_config = texttospeech.AudioConfig(
     audio_encoding=texttospeech.AudioEncoding.MP3
 )
 
+def debug(action, description=None):
+    now = datetime.datetime.now()
+    if description:
+        print(f'[{now.strftime("%d/%m %H:%M")}] {action}: "{description}"')
+    else:
+        print(f'[{now.strftime("%d/%m %H:%M")}] {action}')
+
 def create_voice(msg):
     synthesis_input = texttospeech.SynthesisInput(ssml=msg)
     response = client.synthesize_speech(input=synthesis_input, voice=voice, audio_config=audio_config)
@@ -27,20 +35,24 @@ def speak(msg):
         create_voice(msg)
         playsound.playsound('./voice.mp3')
     except Exception as e:
-        print(f'error: fail to speak - {e}')
+        debug('error', f'fail to speak with {e}')
 
 def bot():
-    print('waiting messages ...')
+    debug('waiting message...')
     while True:
         _, response = redis_server.brpop('msg', timeout=0)
         if response:
             msg = response.decode('utf-8')
-            print(f'saying: "{msg}"')
+            debug('saying', msg)
             speak(f'<speak><break time="1s"/>{msg.capitalize()}</speak>')
 
 
 if __name__ == '__main__':
-    try:
-        bot()
-    except KeyboardInterrupt:
-        print('bye !')
+    while True:
+        try:
+            bot()
+        except KeyboardInterrupt:
+            debug('quit')
+            break
+        except Exception as e:
+            debug('error', e)
